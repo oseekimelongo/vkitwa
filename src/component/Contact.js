94f042b54cfb8d3500153343e83aac9480e275e5
oseekimelongo <146938203+oseekimelongo@users.noreply.github.com>
2024-05-24 16:16:15 +0200

    Add files via upload

---
+++ src/component/Contact.js
@@ -0,0 +1,19 @@
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function contact() {
+    
+    return (
+       
+        
+        
+      <div className='pl-2'>
+       
+      </div>
+     
+    );
+  }
+  
+  export default contact;
+  
